27f62cf8e083d68dfc91c80326dbbf4f9e42e640
susymx <87504129+susymx@users.noreply.github.com>
2022-02-25 11:38:46 -0600

    Update nullModel.py

--- nullModel.py
+++ nullModel.py
@@ -32,75 +32,7 @@ def validPoint(h, k, rx, ry, x, y):
         return True     # Inside the ellipse
     else:
         return False    # Out of the ellipse
-
-# This function creates a list of Cells. total is the total amount of cells to creates
-# It creates them inside of an ellipse with minor diameter of rx and mayor diameter of ry
-# Also, creates the radius of each cell at random or at constant value
-# Receives the number of cells to create, a boolean value to indicate random radius, and coordinates
-# Returns a list of null cells, just like the one we get when reading real values
-def generateCells3D(total, randRad, rx, ry,nsegment,sliceDistance):
-    h = rx/2                                # Calculates the center of the ellipse, no negative
-    k = ry/2                                # Calculates the center of the ellipse
-    nullCells = []                          # List of null cells
-    nullCells.append(Cell3D(0, 0, 0, 0, 0, 0))   # Foo cell, at nullCells[0]
-    for s in range(nsegment): 
-     coordZ = s*sliceDistance
-     for i in range(0, total[s]):               # Iterates over the given range
-        name = i *(s+1)                           # To get the name of each cell
-        if randRad:                         # True if we want random radius
-            radius = random.randint(125, 150)   # Random radius
-        else:
-            radius = 125                        # Fixed value
-        coordX = 0                          # Initial foo coords at x
-        coordY = 0 
-                                # Initial foo coords at y
-        while not validPoint(h, k, rx, ry, coordX, coordY): # Find a valid coord
-            coordX = random.randint(0, rx)                      # Generates at random the coords
-            coordY = random.randint(0, ry)
-        newCell = Cell3D(coordX, coordY,coordZ, name, s, radius) # When valid coord, creates a new cell
-        nullCells.append(newCell)                           # Append that cell to the list
-    return nullCells                        # Return the cells
-
-# This function creates a list of Cells. 
-# It creates them inside of an ellipse with minor diameter of rx and mayor diameter of ry, 
-# and a deepness of nSlice*sliceDistance
-# Also, creates the radius of each cell at random or at constant value
-# Receives the number of cells to create, a boolean value to indicate random radius, and coordinates
-# Returns a list of null cells, just like the one we get when reading real values
-# nSlice is the number of slices, sliceDistance is the distance between each slice
-# The boolean ctrl choose a equation for the number of cells in each slice. True means that the model is control.
-# False means that the model is AVP.
-# Y=B0 + B1*X + B2*X^2. Interpolation with four slices. X is the slice number from rostral to caudal. 
-# Y is the number of cells that will be in the sliceDistance
-def generateCell3DS(randRad,rx,ry,nSlice,sliceDistance,ctrl):
-    h = rx/2                                # Calculates the center of the ellipse, no negative
-    k = ry/2 
-    nullCells =[]
-    nullCells.append(Cell3D(0,0,0,0,0,0))
-    name=1
-    for s in range(nSlice): 
-     rz=(s+1)
-     if ctrl:
-         c=round( 371.2 - 96.64*rz + 17.56*(rz**2))
-     else:
-         c=round( 195.6 + 204.6*rz - 33.75*(rz**2))
-     for i in range(c):               # Iterates over the given range
-        if randRad:                         # True if we want random radius
-            radius = random.randint(125, 150)   # Random radius
-        else:
-            radius = 125                        # Fixed value
-        coordX = 0                          # Initial foo coords at x
-        coordY = 0 
-                                # Initial foo coords at y
-        while not validPoint(h, k, rx, ry, coordX, coordY): # Find a valid coord
-            coordX = random.randint(0, rx)                      # Generates at random the coords
-            coordY = random.randint(0, ry)
-            coordZ = random.randint((rz-1)*sliceDistance, rz*sliceDistance-1)
-        newCell = Cell3D(coordX, coordY,coordZ, name, s, radius) # When valid coord, creates a new cell
-        nullCells.append(newCell)                           # Append that cell to the list
-        name=name+1
-    return nullCells
-
+    
 # This function creates a list of Cells. 
 # It creates them inside of an ellipse with minor diameter of rx and mayor diameter of ry, 
 # and a deepness of nSlice*sliceDistance
